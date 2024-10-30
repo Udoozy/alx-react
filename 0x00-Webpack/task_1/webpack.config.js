8ae60f62be8a7a3e07e4b398e6fea412b3969a9f
Udoozy <agbo4samson@gmail.com>
2024-10-30 03:34:04 +0000

    task_1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,16 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-    template: './public/index.html',
-    filename: 'index.html',
-    })
-  ]
+  mode: 'production'
 };
