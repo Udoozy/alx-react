591ca48e7b4cecc47e4d9c868929c853dc90756a
Udoozy <agbo4samson@gmail.com>
2024-11-06 14:47:13 +0000

    dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,7 +6,7 @@ let count = 0;
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 // $('button').on('click', updateCounter)with debounce;
