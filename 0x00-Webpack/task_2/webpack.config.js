4aa6eeb71aefb510a9e71a4e4685ad76ae28d9a0
Udoozy <agbo4samson@gmail.com>
2024-11-07 15:36:42 +0000

    taske2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -10,5 +10,42 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-  }
-}
+  },
+  module: {
+    rules: [
+      {
+	test: /\.css$/i,
+	use: ['style-loader', 'css-loader'],
+      },
+      {
+	test: /\.(png|jpg|jpeg|gif)$/i,
+	type: 'asset/resource',
+	use: [
+	  {
+	    loader: 'image-webpack-loader',
+	    options: {
+	      disable: process.env.NODE_ENV === 'development',
+	      mozjpeg: {
+		progressive: true,
+		quality: 65,
+	      },
+	      optipng: {
+		enabled: false,
+	      },
+	      pngquant: {
+		qaulity: [0.65, 0.9],
+		speed: 4,
+	      },
+	      gifsicle: {
+		interlaced: false,
+	      },
+	      webp: {
+		quality: 75,
+	      },
+	    },
+	  },
+	],
+      },
+    ],
+  },
+};
